1dd6cbe11740f5434f40e7ac73994bebe03c77a1
zhoujianye <111@123.com>
2018-01-03 17:12:20 +0800

    去掉不必要的文件

--- resources/views/admin/setting/clearcache.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.public')
-
-@section('bodyattr')class="gray-bg"@endsection
-
-@section('body')
-    <div class="middle-box text-center">
-        <h2 class="font-bold">更新成功</h2>
-    </div>
-@endsection
